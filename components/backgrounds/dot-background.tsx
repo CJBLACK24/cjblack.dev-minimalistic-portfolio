e1922ff310282d4755f7c3a5a626255d6c267966
DUQUE CHRISTIAN JOHN C <duquechristianjohncalderon@gmail.com>
2026-01-26 15:58:52 +0800

    feat: Add a new `DotBackground` component for a customizable dot pattern background.

--- components/backgrounds/dot-background.tsx
+++ components/backgrounds/dot-background.tsx
@@ -22,15 +22,15 @@ export function DotBackground({
       <div
         className={cn(
           "absolute inset-0 z-0",
-          "[background-size:20px_20px]",
-          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
-          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+          "bg-size-[20px_20px]",
+          "bg-[radial-gradient(#d4d4d4_1px,transparent_1px)]",
+          "dark:bg-[radial-gradient(#404040_1px,transparent_1px)]",
           dotColor &&
             `[background-image:radial-gradient(${dotColor}_1px,transparent_1px)]`,
         )}
       />
       {/* Radial gradient for the container to give a faded look */}
-      <div className="pointer-events-none absolute inset-0 z-10 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      <div className="pointer-events-none absolute inset-0 z-10 flex items-center justify-center bg-white mask-[radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
       <div className="relative z-20 w-full">{children}</div>
     </div>
   );
